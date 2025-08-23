d926aa920b96c91b7922d2bb95149c9c35176cb7
Sumit Chaturvedi <sumitchaturvedilucky2002@gmail.com>
2025-08-23 18:01:13 +0530

    Push: Home, Hotels, Hotel Details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route,Routes,useLocation } from 'react-router-dom'
 import Home from './pages/Home';
+import Footer from './components/Footer';
+import Rooms from './pages/Rooms';
+import RoomDetails from './pages/RoomDetails';
 
 const App = () => {
   const hotelAdmin= useLocation().pathname.includes("hotel-admin");
@@ -11,8 +14,11 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/rooms' element={<Rooms/>}/>
+          <Route path='/rooms/:id' element={<RoomDetails/>}/>
         </Routes>
       </div>
+      <Footer/>
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Destination from '../components/Destination'
+import Offers from '../components/Offers'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
     <>
         <Hero/>
+        <Destination/>
+        <Offers/>
+        <Testimonial/>
+        <NewsLetter/>
     </>
   )
 }
